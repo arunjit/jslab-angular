0df6304b45d84009387a84b7bdbc6a650e0885c7
Arunjit Singh <opensrc@ajsd.in>
2013-07-16 22:20:47 +0530

    more testable and update UI

--- js/controllers/multiselect-ctrl.js
+++ js/controllers/multiselect-ctrl.js
@@ -1,24 +1,35 @@
 angular.module('main').controller('MultiSelectCtrl',
     function($scope, $http) {
       var list = $scope.list = [];
+      $scope.model = {
+        filter: ''
+      };
 
       $http.get('data/products.json').success(function(d) {
-        $scope.list = d;
+        list.push.apply(list, d);
       });
 
-      $scope.isSelected = function(product) {
+      var isSelected = $scope.isSelected = function(product) {
         return !!product.selected;
+      };
+
+      $scope.countSelected = function() {
+        return list.filter(isSelected).length;
       };
 
       $scope.toggle = function(product) {
         product.selected = !product.selected;
       };
 
-      $scope.deselect = function(product) {
+      var deselect = $scope.deselect = function(product) {
         product.selected = false;
       };
 
-      $scope.saveSelections = function() {
+      $scope.clear = function() {
+        list.forEach(deselect);
+      };
+
+      $scope.save = function() {
         var selections = list.filter(isSelected);
         console.log("SAVE:", selections);
       };
