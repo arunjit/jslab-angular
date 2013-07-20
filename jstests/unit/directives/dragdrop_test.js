4fadf7b389eed81446dc9b22a05a8aa246537456
Arunjit Singh <opensrc@ajsd.in>
2013-07-20 21:51:39 +0530

    add first directive test

---
+++ jstests/unit/directives/dragdrop_test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+describe('aj-dragdrop', function() {
+
+  beforeEach(module('main'));
+
+  var element, scope;
+
+  describe('aj-drag', function() {
+    beforeEach(inject(function($compile, $rootScope) {
+      element = angular.element('<div aj-drag="x"></div>');
+      scope = $rootScope.$new();
+      scope.x = 20;
+      $compile(element)(scope);
+      scope.$digest();
+    }));
+
+    it('should have been set up', function() {
+      expect(element.attr('draggable')).toEqual('true');
+    });
+  });
+});
