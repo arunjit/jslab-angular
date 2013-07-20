603847931ab38f15f90346623e11af42910d3a2d
Arunjit Singh <opensrc@ajsd.in>
2013-07-20 22:51:12 +0530

    add and then ignore a real test

--- jstests/unit/directives/dragdrop_test.js
+++ jstests/unit/directives/dragdrop_test.js
@@ -11,12 +11,18 @@ describe('aj-dragdrop', function() {
       element = angular.element('<div aj-drag="x"></div>');
       scope = $rootScope.$new();
       scope.x = 20;
-      $compile(element)(scope);
+      element = $compile(element)(scope);
       scope.$digest();
     }));
 
     it('should have been set up', function() {
       expect(element.attr('draggable')).toEqual('true');
+    });
+
+    // Ignore till event on triggerHandler can be fixed.
+    xit('should update classes on dragstart', function() {
+      element.triggerHandler('dragstart');
+      expect(element.hasClass('dragstart')).toBe(true);
     });
   });
 });
